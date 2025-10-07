87d31c9122a381a657c1c16a13a901100a94b530
John Leo <leo@halfaya.org>
2025-10-07 09:55:59 -0700

    Start Lemma 3.2

--- Monogenic.lean
+++ Monogenic.lean
@@ -2,3 +2,17 @@ import Monogenic.Basic
 import Mathlib
 
 #eval Lean.versionString
+
+#eval 3+4
+
+#check IsLocalRing.maximal_ideal_unique
+
+
+lemma lemma_3_2 (R S : Type)
+  [CommRing R] [CommRing S]
+  [IsLocalRing R] [IsLocalRing S]
+  --[Algebra R S]
+  (ϕ : R →+* S)
+  (étale : RingHom.Etale ϕ) :
+  (∃ β : S, Algebra.adjoin ϕ.range {β} = S) ∧
+  ∃ (f : Polynomial R) (fmon : f.Monic), Polynomial.aeval β f = 0
