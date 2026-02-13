56a5e0045d3871cbe7f81aea5d1ef762280f9996
George Peykanu <gpeyka@uw.edu>
2026-02-12 16:40:29 -0800

    Basic Imports removal

--- Monogenic/Basic.lean
+++ Monogenic/Basic.lean
@@ -1,6 +1,3 @@
-import Mathlib.RingTheory.AdjoinRoot
-import Mathlib.Algebra.Algebra.Subalgebra.Basic
-import Mathlib.RingTheory.Ideal.Quotient.Operations
 import Mathlib.RingTheory.IsAdjoinRoot
 
 open Polynomial
